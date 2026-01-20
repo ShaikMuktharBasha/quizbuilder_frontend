9fda5ef57a525af92db8e90c32f0806263dc89ed
Shaik Mukhtar Basha <2300032055@kluniversity.in>
2026-01-20 18:27:55 +0530

    UI Improvements: Add close button and AI notification bubble

--- src/QuizChatbot.jsx
+++ src/QuizChatbot.jsx
@@ -158,14 +158,18 @@ export default function QuizChatbot({ quiz, setQuiz, questions, setQuestions, se
 
   if (!isOpen) {
     return (
-      <button className="chatbot-toggle" onClick={() => setIsOpen(true)}>
-        <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            {/* Using the RobotIconSimple or just an SVG for chat */}
-             <svg width="30" height="30" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H6L4 18V4H20V16Z" fill="white"/>
-            </svg>
-        </div>
-      </button>
+      <div className="chatbot-closed-wrapper">
+         <div className="chatbot-tooltip" onClick={() => setIsOpen(true)}>
+            <span>✨ Generate Quiz with AI</span>
+         </div>
+         <button className="chatbot-toggle" onClick={() => setIsOpen(true)}>
+            <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <svg width="30" height="30" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H6L4 18V4H20V16Z" fill="white"/>
+                </svg>
+            </div>
+         </button>
+      </div>
     );
   }
 
